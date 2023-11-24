ee73e34c2f33d154d6bf7b55020ac9b9ede0f057
ASanderD <Stepmini1989@gmail.com>
2023-11-24 22:59:54 +0700

    Initial commit

---
+++ collections-lists-and-sets/src/main/java/skyprocollectionslistsandsets/CollectionsListsAndSetsApplication.java
@@ -0,0 +1,13 @@
+package skyprocollectionslistsandsets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsListsAndSetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsListsAndSetsApplication.class, args);
+	}
+
+}
